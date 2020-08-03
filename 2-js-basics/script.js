a089d396ddb9e3c0133bea57c7aa71998bf0925c
Moriah Humphries <moriah.humphries@gmail.com>
2020-08-03 14:20:13 -0500

    review of type coercion, user input, variables

---
+++ 2-js-basics/script.js
@@ -0,0 +1,28 @@
+// Variables and Data Types
+// console.log("Hello World from external js!");
+
+// let firstName = "Moriah";
+// console.log(firstName);
+//
+// let age = 32;
+// let fullAge = true;
+
+// Variables and Type Coercion
+
+let firstName = "Moriah";
+let age = 32;
+let fullAge = true;
+
+console.log(firstName + " is " + age + "!");
+
+console.log(typeof firstName + " " + typeof age);
+
+alert(firstName + " is " + age + "!");
+
+let userAge = prompt("What is your age?");
+console.log("Users age = " + userAge);
+
+
+
+
+
