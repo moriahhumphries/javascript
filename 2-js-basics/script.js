918cda9cda4171a57a970ff5868f1c50bcd2d9bd
Moriah Humphries <moriah.humphries@gmail.com>
2020-08-03 15:02:46 -0500

    operator precedence practice

--- 2-js-basics/script.js
+++ 2-js-basics/script.js
@@ -24,19 +24,41 @@
 
 //Operators
 
-let currentYear = 2020;
-let moriahAge = 32;
-let andrewAge = 35;
-let yearMoriah = currentYear - moriahAge;
-console.log("Moriah was born in: " + yearMoriah);
+// let currentYear = 2020;
+// let moriahAge = 32;
+// let andrewAge = 35;
+// let yearMoriah = currentYear - moriahAge;
+// console.log("Moriah was born in: " + yearMoriah);
+//
+// console.log(currentYear + 2);
+// console.log(currentYear - 2);
+// console.log(currentYear / 2);
+// console.log(currentYear * 2);
+//
+// let andrewOlder = moriahAge < andrewAge;
+// console.log("Is Andrew older than Moriah? " + andrewOlder);
+//
+// console.log(typeof currentYear);
+// console.log(typeof moriahAge);
+// console.log(typeof andrewAge);
+// console.log(typeof andrewOlder);
 
-console.log(currentYear + 2);
-console.log(currentYear - 2);
-console.log(currentYear / 2);
-console.log(currentYear * 2);
+//Operator Precedence
 
-let andrewOlder = moriahAge < andrewAge;
-console.log("Is Andrew older than Moriah? " + andrewOlder);
+let now = 2020;
+let yearMoriah = 1988;
+let fullAge = 18;
+
+var isFullAge = (now - yearMoriah) >= fullAge;
+
+console.log(isFullAge)
+
+let ageAndrew = 35;
+let ageMoriah = 32;
+let averageAge = (ageAndrew + ageMoriah) / 2;
+
+console.log(averageAge);
+
 
 
 
