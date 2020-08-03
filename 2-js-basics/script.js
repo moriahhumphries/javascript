33c351035d547eec22d72b2365eb69af691d8b11
Moriah Humphries <moriah.humphries@gmail.com>
2020-08-03 14:26:40 -0500

    math operators and boolean value practice

--- 2-js-basics/script.js
+++ 2-js-basics/script.js
@@ -9,20 +9,34 @@
 
 // Variables and Type Coercion
 
-let firstName = "Moriah";
-let age = 32;
-let fullAge = true;
+// let firstName = "Moriah";
+// let age = 32;
+// let fullAge = true;
+//
+// console.log(firstName + " is " + age + "!");
+//
+// console.log(typeof firstName + " " + typeof age);
+//
+// alert(firstName + " is " + age + "!");
+//
+// let userAge = prompt("What is your age?");
+// console.log("Users age = " + userAge);
 
-console.log(firstName + " is " + age + "!");
+//Operators
 
-console.log(typeof firstName + " " + typeof age);
+let currentYear = 2020;
+let moriahAge = 32;
+let andrewAge = 35;
+let yearMoriah = currentYear - moriahAge;
+console.log("Moriah was born in: " + yearMoriah);
 
-alert(firstName + " is " + age + "!");
+console.log(currentYear + 2);
+console.log(currentYear - 2);
+console.log(currentYear / 2);
+console.log(currentYear * 2);
 
-let userAge = prompt("What is your age?");
-console.log("Users age = " + userAge);
-
-
+let andrewOlder = moriahAge < andrewAge;
+console.log("Is Andrew older than Moriah? " + andrewOlder);
 
 
 
