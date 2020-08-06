27b95029d4b0500ab7054d264c87a925d895c89f
Moriah Humphries <moriah.humphries@gmail.com>
2020-08-06 15:08:56 -0500

    creating dice roll function

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -9,10 +9,27 @@ GAME RULES:
 
 */
 console.log('test');
-var scores, roundScore, activePlayer, dice;
+let scores, roundScore, activePlayer, dice;
 
 scores = [0,0];
 roundScore = 0;
 activePlayer = 0;
 
-dice
+console.log(dice);
+
+document.querySelector("#current-"  + activePlayer).textContent = dice;
+// document.querySelector("#current-" + activePlayer).innerHTML = "<em>" + dice + "</em>"
+
+
+document.querySelector(".dice").style.display = "none";
+
+document.querySelector("btn-roll").addEventListener("click", function (){
+    dice = Math.floor(Math.random() * 6) + 1;
+
+});
+
+
+
+
+
+
