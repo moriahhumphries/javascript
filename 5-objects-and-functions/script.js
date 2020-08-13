bc3dba93c8710bf92ec285103d9d9fa57a34294c
Moriah Humphries <moriah.humphries@gmail.com>
2020-08-13 14:43:47 -0500

    Added another prototype example

--- 5-objects-and-functions/script.js
+++ 5-objects-and-functions/script.js
@@ -14,9 +14,12 @@ let Person = function(firstName, birthYear, job) {
     this.job = job;
 }
 
+// Will be inherited for all instances of Person
 Person.prototype.calculateAge = function() {
     console.log(2020 - this.birthYear);
 }
+
+Person.prototype.lastName = "Smith";
 
 let kate = new Person("Kate", 1990, "teacher");
 let jane = new Person("Jane", 1968, "designer");
@@ -25,5 +28,9 @@ let mark = new Person("Mark", 1948, "retired");
 kate.calculateAge();
 jane.calculateAge();
 mark.calculateAge();
+
+console.log(kate.lastName);
+console.log(jane.lastName);
+console.log(mark.lastName);
 
 
