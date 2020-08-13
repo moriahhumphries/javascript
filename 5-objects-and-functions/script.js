078768c9df891fd05dced8f734c3912cc6e3b6a5
Moriah Humphries <moriah.humphries@gmail.com>
2020-08-13 17:01:38 -0500

    completed functions returning functions

--- 5-objects-and-functions/script.js
+++ 5-objects-and-functions/script.js
@@ -58,80 +58,109 @@
 // Primitives vs objects
 
 // Primitives
-let a = 23;
-let b = a;
-a = 46;
-
-console.log(a);
-console.log(b);
-
-let obj1 = {
-    name: "John",
-    age: 26
-};
-
-// points to obj1
-let obj2 = obj1;
-obj1.age = 30;
-
-console.log(obj1.age);
-console.log(obj2.age);
-
-// Functions
-
-let age = 32;
-let obj = {
-    name: "Moriah",
-    city: "Dallas"
-};
-
-function change(a, b){
-    a = 30; // primitive
-    b.city = "San Francisco"; // Changes city, object passed
-}
-
-change(age, obj);
-
-console.log(age);
-console.log(obj.city);
+// let a = 23;
+// let b = a;
+// a = 46;
+//
+// console.log(a);
+// console.log(b);
+//
+// let obj1 = {
+//     name: "John",
+//     age: 26
+// };
+//
+// // points to obj1
+// let obj2 = obj1;
+// obj1.age = 30;
+//
+// console.log(obj1.age);
+// console.log(obj2.age);
+//
+// // Functions
+//
+// let age = 32;
+// let obj = {
+//     name: "Moriah",
+//     city: "Dallas"
+// };
+//
+// function change(a, b){
+//     a = 30; // primitive
+//     b.city = "San Francisco"; // Changes city, object passed
+// }
+//
+// change(age, obj);
+//
+// console.log(age);
+// console.log(obj.city);
 
 // Passing Functions as Arguments
 
-let years = [1998, 1988, 2005, 1984, 1972];
+// let years = [1998, 1988, 2005, 1984, 1972];
+//
+// function arrayCalc(arr, fn) {
+//     let arrRes = [];
+//     for(let i = 0; i < arr.length; i++) {
+//         arrRes.push(fn(arr[i])); // inserts element at end of array
+//     }
+//     return arrRes;
+// }
+//
+// // Callback functions, functions passed into functions
+//
+// function calculateAge(ele) {
+//     return 2020 - ele;
+// }
+//
+// function fullAge(ele) {
+//     return ele >= 18;
+// }
+//
+// function maxHeartRate(ele) {
+//     if(ele >= 18 && ele <= 81) {
+//         return Math.round(206.9 - (0.67 * ele));
+//     }
+//     return -1;
+// }
+//
+// let ages = arrayCalc(years, calculateAge); // not calling function, passing in
+// let fullAges = arrayCalc(ages, fullAge); // uses array, passes in function
+// let rates = arrayCalc(ages, maxHeartRate);
+//
+// console.log(ages);
+// console.log(fullAges);
+// console.log(rates);
 
-function arrayCalc(arr, fn) {
-    let arrRes = [];
-    for(let i = 0; i < arr.length; i++) {
-        arrRes.push(fn(arr[i])); // inserts element at end of array
+
+// Functions returning functions
+
+function interviewQuestion(job){
+    if(job === "designer"){
+        return function (name) { // anonymous function
+            console.log(name + ", can you explain what UX design is?")
+        }
+    } else if (job === "teacher") {
+        return function (name) {
+            console.log("What subject do you teach, " + name + "?")
+        }
+    } else {
+        return function (name) {
+            console.log("Hello, " + name + " what do you do?")
+        }
     }
-    return arrRes;
 }
 
-// Callback functions, functions passed into functions
+// functions are objects
+let teacherQuestion = interviewQuestion("teacher"); // "teacher" anon function
+let designerQuestion = interviewQuestion("designer"); // "designer anon function
 
-function calculateAge(ele) {
-    return 2020 - ele;
-}
+teacherQuestion("John");
+designerQuestion("John");
+teacherQuestion("Jane");
+designerQuestion("Sandra");
 
-function fullAge(ele) {
-    return ele >= 18;
-}
-
-function maxHeartRate(ele) {
-    if(ele >= 18 && ele <= 81) {
-        return Math.round(206.9 - (0.67 * ele));
-    }
-    return -1;
-}
-
-let ages = arrayCalc(years, calculateAge); // not calling function, passing in
-let fullAges = arrayCalc(ages, fullAge); // uses array, passes in function
-let rates = arrayCalc(ages, maxHeartRate);
-
-console.log(ages);
-console.log(fullAges);
-console.log(rates);
-
+interviewQuestion("teacher")("Mark"); // evaluates from left to right
 
 
 
